1e623190d84aeec8c1cf56fb97316bafb85a3a75
Skyler Nesheim <skyler@dwolla.com>
2015-11-28 15:34:28 -0600

    MASTER: 02-count-characters : when input is empty, repeatedly ask for input

--- 02-count-characters/elixir_count_characters/lib/elixir_count_characters.ex
+++ 02-count-characters/elixir_count_characters/lib/elixir_count_characters.ex
@@ -1,10 +1,18 @@
 defmodule ElixirCountCharacters do
 
   def main(_args) do
-    IO.gets("What is the input string? ")
-    |> String.strip
+    get_input
     |> create_output
     |> IO.puts
+  end
+
+  defp get_input() do
+    input = IO.gets("What is the input string? ")
+    |> String.strip
+    case input do
+      "" -> get_input
+      not_empty -> not_empty
+    end
   end
 
   defp create_output(input) do
