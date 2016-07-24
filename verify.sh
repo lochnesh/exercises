88c08a206a696d4d7778c7afeea6e9de9c5458a0
Skyler Nesheim <skyler@dwolla.com>
2016-07-23 21:57:49 -0500

    MASTER: add script to verify all exercises

---
+++ verify.sh
@@ -0,0 +1,19 @@
+set -o pipefail
+set -e
+
+#01
+pushd 01-say-hello/elixir_say_hello && mix test && popd
+
+#02
+pushd 02-count-characters/elixir_count_characters && mix test && popd
+
+#03
+pushd 03-printing-quotes/elixir_printing_quotes && mix test && popd
+
+#04
+pushd 04-mad-lib/elixir_mad_lib && mix test && popd
+
+#05
+pushd 05-simple-math/scala-simple-math && sbt test && popd
+
+
