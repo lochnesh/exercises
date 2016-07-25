932d859e92204cd6ca50e27b951f9f05cdde4a66
Skyler Nesheim <skyler@dwolla.com>
2016-07-25 12:09:49 -0500

    MASTER: extract functions for language verification

--- verify.sh
+++ verify.sh
@@ -1,17 +1,27 @@
 set -o pipefail
 set -e
 
+verify_elixir()
+{
+  mix test && mix credo --strict
+}
+
+verify_scala()
+{
+  sbt scalastyle test:scalastyle test
+}
+
 #01
-pushd 01-say-hello/elixir_say_hello && mix test && mix credo --strict && popd
+pushd 01-say-hello/elixir_say_hello && verify_elixir && popd
 
 #02
-pushd 02-count-characters/elixir_count_characters && mix test && mix credo --strict  && popd
+pushd 02-count-characters/elixir_count_characters && verify_elixir  && popd
 
 #03
-pushd 03-printing-quotes/elixir_printing_quotes && mix test && mix credo --strict && popd
+pushd 03-printing-quotes/elixir_printing_quotes && verify_elixir && popd
 
 #04
-pushd 04-mad-lib/elixir_mad_lib && mix test && mix credo --strict && popd
+pushd 04-mad-lib/elixir_mad_lib && verify_elixir && popd
 
 #05
-pushd 05-simple-math/scala-simple-math && sbt scalastyle test:scalastyle test && popd
+pushd 05-simple-math/scala-simple-math && verify_scala && popd
