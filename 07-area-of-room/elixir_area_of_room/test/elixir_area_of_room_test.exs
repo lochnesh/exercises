eeb7df8136842f8cdc1df1759f528709dab5118c
Skyler Nesheim <skyler@dwolla.com>
2016-07-29 14:48:06 -0500

    MASTER: elixir_area_of_room : refactor test to not use attributes

--- 07-area-of-room/elixir_area_of_room/test/elixir_area_of_room_test.exs
+++ 07-area-of-room/elixir_area_of_room/test/elixir_area_of_room_test.exs
@@ -3,26 +3,25 @@ defmodule ElixirAreaOfRoomTest do
   import ExUnit.CaptureIO
   doctest ElixirAreaOfRoom
 
-  @length 15.0
-  @width 20.0
-
   test "run produces expected results" do
+    length = 15.0
+    width = 20.0
+    read = fn(prompt) ->
+      case prompt do
+        "What is the length of the room in feet? " -> "#{length}\n"
+        "What is the width of the room in feet? " -> "#{width}\n" 
+      end
+    end
+
     assert capture_io(fn ->
-      ElixirAreaOfRoom.run(&read/1)
+      ElixirAreaOfRoom.run(read)
     end
     ) == """
-    You entered the dimensions of #{@length} feet by #{@width} feet.
+    You entered the dimensions of #{length} feet by #{width} feet.
     The area is
     300.0 square feet
     27.871 square meters
     """
-  end
-
-  def read(prompt) do
-    case prompt do
-      "What is the length of the room in feet? " -> "#{@length}\n"
-      "What is the width of the room in feet? " -> "#{@width}\n" 
-    end
   end
 
 end
