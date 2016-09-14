ad35e3bb24f173184e1c1c94c56ed5920ec7e237
Skyler Nesheim <skylernesheim@gmail.com>
2016-09-13 22:02:57 -0500

    MASTER: scala-paint-calculator : prompt user for length and width of the ceiling

--- 09-paint-calculator/scala-paint-calculator/src/main/scala/com/lochnesh/paint/calculator/Main.scala
+++ 09-paint-calculator/scala-paint-calculator/src/main/scala/com/lochnesh/paint/calculator/Main.scala
@@ -2,12 +2,15 @@ package com.lochnesh.paint.calculator
 
 object Main extends App {
 
-  PaintCalculator.run()
+  val input = (prompt: String) ⇒ scala.io.StdIn.readLine(prompt)
+  PaintCalculator.run(input)
 
 }
 
 object PaintCalculator {
-  def run(): Unit = {
+  def run(input: (String) ⇒ String): Unit = {
+    val length = input("What is the length of the ceiling? ").toDouble
+    val width = input("What is the width of the ceiling? ").toDouble
 
   }
 }
--- 09-paint-calculator/scala-paint-calculator/src/test/scala/com/lochnesh/paint/calculator/MainSpec.scala
+++ 09-paint-calculator/scala-paint-calculator/src/test/scala/com/lochnesh/paint/calculator/MainSpec.scala
@@ -5,8 +5,14 @@ import org.scalatest.{Matchers, FlatSpec}
 class MainSpec extends FlatSpec with Matchers {
 
   it should "prompt user for length and width and print number of gallons" in {
+    val inputs = (p: String) ⇒ {
+      p match {
+        case "What is the length of the ceiling? " ⇒ "12"
+        case "What is the width of the ceiling? " ⇒ "10"
+      }
+    }
 
-    PaintCalculator.run()
+    PaintCalculator.run(inputs)
   }
 
 }
