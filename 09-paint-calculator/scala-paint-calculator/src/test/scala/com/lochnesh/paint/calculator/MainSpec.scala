76c3247793f611ca16fd7fb9a4c5f8972dd7a0a9
Skyler Nesheim <skylernesheim@gmail.com>
2016-09-14 22:41:13 -0500

    MASTER: scala-paint-calculator : use promises and async specs to replace vals when capturing output

--- 09-paint-calculator/scala-paint-calculator/src/test/scala/com/lochnesh/paint/calculator/MainSpec.scala
+++ 09-paint-calculator/scala-paint-calculator/src/test/scala/com/lochnesh/paint/calculator/MainSpec.scala
@@ -1,31 +1,27 @@
 package com.lochnesh.paint.calculator
 
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.{AsyncFlatSpec, Matchers}
 
-class MainSpec extends FlatSpec with Matchers {
+import scala.concurrent.Promise
+
+class MainSpec extends AsyncFlatSpec with Matchers {
 
   it should "prompt user for length and width and print number of gallons" in {
 
-    var data = Seq[String]()
-    val output: (String) ⇒ Unit = (t: String) ⇒ {
-      data = data :+ t
-    }
+    val (result, func) = output()
 
-    PaintCalculator.run(inputs("12", "10"), output)
+    PaintCalculator.run(inputs("12", "10"), func)
 
-    data.head should be ("You will need 1 gallon of paint to cover 120.0 square feet.")
+    result.future map { _ should be ("You will need 1 gallon of paint to cover 120.0 square feet.") }
   }
 
   it should "pluralize response" in {
 
-    var data = Seq[String]()
-    val output: (String) ⇒ Unit = (t: String) ⇒ {
-      data = data :+ t
-    }
+    val (result, func) = output()
 
-    PaintCalculator.run(inputs("20", "30"), output)
+    PaintCalculator.run(inputs("20", "30"), func)
 
-    data.head should be ("You will need 2 gallons of paint to cover 600.0 square feet.")
+    result.future map { _ should be ("You will need 2 gallons of paint to cover 600.0 square feet.") }
   }
 
   private def inputs(length: String, width: String) = {
@@ -35,6 +31,14 @@ class MainSpec extends FlatSpec with Matchers {
         case "What is the width of the ceiling? " ⇒ width
       }
     }
+  }
+
+  private def output() = {
+    val data = Promise[String]()
+    val output: (String) ⇒ Unit = (t: String) ⇒ {
+      data.success(t)
+    }
+    (data, output)
   }
 
 }
