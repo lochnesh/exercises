fa08e6800266dec8202aa5d502f338eb86df4940
Skyler Nesheim <skylernesheim@gmail.com>
2016-09-30 09:22:36 -0500

    EBERT-CLEANUP: fix issues found by ebert

--- 07-area-of-room/elixir_area_of_room/test/elixir_area_of_room/area_test.exs
+++ 07-area-of-room/elixir_area_of_room/test/elixir_area_of_room/area_test.exs
@@ -1,9 +1,10 @@
 defmodule ElixirAreaOfRoom.AreaTest do
   use ExUnit.Case
   doctest ElixirAreaOfRoom.Area
+  alias ElixirAreaOfRoom.Area
 
   test "calculates area of room in feet" do
-    assert ElixirAreaOfRoom.Area.in_feet(15, 20) == 300
+    assert Area.in_feet(15, 20) == 300
   end
 
 end
--- 07-area-of-room/elixir_area_of_room/test/elixir_area_of_room/conversion_test.exs
+++ 07-area-of-room/elixir_area_of_room/test/elixir_area_of_room/conversion_test.exs
@@ -1,9 +1,10 @@
 defmodule ElixirAreaOfRoom.ConversionTest do
   use ExUnit.Case
   doctest ElixirAreaOfRoom.Conversion
+  alias ElixirAreaOfRoom.Conversion
 
   test "converts feet to meters" do
-    assert ElixirAreaOfRoom.Conversion.feet_to_meters(300) == 27.871
+    assert Conversion.feet_to_meters(300) == 27.871
   end
 
 end
