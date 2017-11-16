fba5edd3260ec714b1f89282894180cf64bc314e
Skyler Nesheim <skylernesheim@gmail.com>
2017-11-15 22:11:34 -0600

    add tax functions to python-self-checkout

--- 10-self-checkout/python-self-checkout/checkout/__init__.py
+++ 10-self-checkout/python-self-checkout/checkout/__init__.py
@@ -2,12 +2,19 @@ class Register:
 
     def __init__(self):
         self.items = []
+        self.__tax_rate = .055
 
     def add_item(self, item, price, quantity):
         self.items.append((item, price, quantity))
 
     def subtotal(self):
         return reduce(self.__total_func, self.items, 0)
+
+    def tax(self):
+        def round_up(subtotal):
+            return round(subtotal, 2)
+
+        return round_up((self.subtotal() * (1 + self.__tax_rate)))
 
     def __total_func(self, total, (item, price, quantity)):
         return total + (price * quantity)
--- 10-self-checkout/python-self-checkout/tests/test_register.py
+++ 10-self-checkout/python-self-checkout/tests/test_register.py
@@ -5,7 +5,22 @@ from checkout import Register
 
 class TestRegister(unittest.TestCase):
 
+    def setUp(self):
+        self.register = Register()
+
+    def add_items(self):
+        self.register.add_item("orange", .50, 3)
+        self.register.add_item("banana", .3, 2)
+
     def test_add_item(self):
-        register = Register()
-        register.add_item("orange", .50, 3)
-        self.assertEqual(1.50, register.subtotal())
+        self.add_items()
+        self.assertEqual(("orange", .50, 3), self.register.items[0])
+        self.assertEqual(("banana", .3, 2), self.register.items[1])
+
+    def test_subtotal(self):
+        self.add_items()
+        self.assertEqual(2.10, self.register.subtotal())
+
+    def test_tax(self):
+        self.add_items()
+        self.assertEqual(2.22, self.register.tax())
