c5fc3c56abe8af8d010e304e90efe16f3147e013
Skyler Nesheim <skylernesheim@gmail.com>
2018-01-27 22:10:55 -0600

    scala-simple-math use cats-effect

--- 05-simple-math/scala-simple-math/src/main/scala/com/lochnesh/exercises/simplemath/Main.scala
+++ 05-simple-math/scala-simple-math/src/main/scala/com/lochnesh/exercises/simplemath/Main.scala
@@ -2,13 +2,21 @@ package com.lochnesh.exercises.simplemath
 
 import com.lochnesh.exercises.simplemath.ComputationImplicits._
 import scala.io.StdIn._
+import cats.effect.IO
 
 object Main extends App {
-  val firstNumber = BigDecimal(readLine("What is the first number? "))
-  val secondNumber = BigDecimal(readLine("What is the second number? "))
 
-  println(s"$firstNumber + $secondNumber = ${firstNumber plus secondNumber}")
-  println(s"$firstNumber - $secondNumber = ${firstNumber minus secondNumber}")
-  println(s"$firstNumber * $secondNumber = ${firstNumber times secondNumber}")
-  println(s"$firstNumber / $secondNumber = ${firstNumber divide secondNumber}")
+  def read(prompt: String) = IO { BigDecimal(readLine(prompt)) }
+  def print(output: String) = IO { println(output) }
+
+  val program = for {
+    x <- read("What is the first number? ")
+    y <- read("What is the second number? ")
+    _ <- print(s"$x + $y = ${x plus y}")
+    _ <- print(s"$x - $y = ${x minus y}")
+    _ <- print(s"$x * $y = ${x times y}")
+    _ <- print(s"$x / $y = ${x divide y}")
+  } yield ()
+
+  program.unsafeRunSync()
 }
