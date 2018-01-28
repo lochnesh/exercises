3b8025a6fb31eb0dab7f241ade7b2ce16f71fae5
Skyler Nesheim <skylernesheim@gmail.com>
2018-01-27 23:01:57 -0600

    scala-simple-math: use ← instead of <-

--- 05-simple-math/scala-simple-math/src/main/scala/com/lochnesh/exercises/simplemath/Main.scala
+++ 05-simple-math/scala-simple-math/src/main/scala/com/lochnesh/exercises/simplemath/Main.scala
@@ -10,12 +10,12 @@ object Main extends App {
   def print(output: String) = IO { println(output) }
 
   val program = for {
-    x <- read("What is the first number? ")
-    y <- read("What is the second number? ")
-    _ <- print(s"$x + $y = ${x plus y}")
-    _ <- print(s"$x - $y = ${x minus y}")
-    _ <- print(s"$x * $y = ${x times y}")
-    _ <- print(s"$x / $y = ${x divide y}")
+    x ← read("What is the first number? ")
+    y ← read("What is the second number? ")
+    _ ← print(s"$x + $y = ${x plus y}")
+    _ ← print(s"$x - $y = ${x minus y}")
+    _ ← print(s"$x * $y = ${x times y}")
+    _ ← print(s"$x / $y = ${x divide y}")
   } yield ()
 
   program.unsafeRunSync()
