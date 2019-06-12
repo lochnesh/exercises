e166f2c160855cd31305b4e4723d1484674336bc
Skyler Nesheim <skylernesheim@gmail.com>
2019-06-12 08:35:40 -0500

    elixir-retirement-calculator: apply mix autoformatting

--- 06-retirement-calculator/elixir_retirement_calculator/lib/elixir_retirement_calculator.ex
+++ 06-retirement-calculator/elixir_retirement_calculator/lib/elixir_retirement_calculator.ex
@@ -1,18 +1,21 @@
 defmodule ElixirRetirementCalculator do
-
   @moduledoc """
   A program that calculates how long until the user retires
   """
 
-  @spec main(list(String.t)) :: :ok
+  @spec main(list(String.t())) :: :ok
   def main(_), do: run(&IO.gets/1, &get_current_year/0)
 
-  @spec run((String.t -> String.t), (() -> String.t)) :: :ok
+  @spec run((String.t() -> String.t()), (() -> String.t())) :: :ok
   def run(input_func, year_func) do
-    age = "What is your current age? "
+    age =
+      "What is your current age? "
       |> get_input(input_func)
-    retirement_age = "At what age would you like to retire? "
+
+    retirement_age =
+      "At what age would you like to retire? "
       |> get_input(input_func)
+
     year = year_func.()
     retirement_year = time_to_retire(age, retirement_age, year)
     years_to_retirement = years_to_retirement(age, retirement_age)
@@ -36,8 +39,7 @@ defmodule ElixirRetirementCalculator do
   defp get_input(prompt, input_func) do
     prompt
     |> input_func.()
-    |> String.trim
-    |> String.to_integer
+    |> String.trim()
+    |> String.to_integer()
   end
-
 end
--- 06-retirement-calculator/elixir_retirement_calculator/test/elixir_retirement_calculator_test.exs
+++ 06-retirement-calculator/elixir_retirement_calculator/test/elixir_retirement_calculator_test.exs
@@ -4,21 +4,20 @@ defmodule ElixirRetirementCalculatorTest do
   doctest ElixirRetirementCalculator
 
   test "calculator prints time to retirement" do
-    year = fn() -> 2016 end
-    assert capture_io(fn ->
-      ElixirRetirementCalculator.run(&(input_fake(&1)), year)
-    end) == """
-    You have 35 years left until you can retire.
-    It's #{year.()}, so you can retire in 2051.
-    """
+    year = fn -> 2016 end
 
+    assert capture_io(fn ->
+             ElixirRetirementCalculator.run(&input_fake(&1), year)
+           end) == """
+           You have 35 years left until you can retire.
+           It's #{year.()}, so you can retire in 2051.
+           """
   end
 
   defp input_fake(input) do
     case input do
-     "What is your current age? " -> "30\n"
-     "At what age would you like to retire? " -> "65\n"
+      "What is your current age? " -> "30\n"
+      "At what age would you like to retire? " -> "65\n"
     end
   end
-
 end
