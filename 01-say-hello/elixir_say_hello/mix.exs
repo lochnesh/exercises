b4704a64373fcc3731954531243fa72bf0ed4bac
Skyler Nesheim <skylernesheim@gmail.com>
2025-09-22 22:10:16 -0500

    Update Elixir project dependencies

--- 01-say-hello/elixir_say_hello/mix.exs
+++ 01-say-hello/elixir_say_hello/mix.exs
@@ -5,7 +5,7 @@ defmodule ElixirSayHello.Mixfile do
     [
       app: :elixir_say_hello,
       version: "0.0.1",
-      elixir: "~> 1.6",
+      elixir: "~> 1.18",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       escript: [main_module: ElixirSayHello],
@@ -21,8 +21,11 @@ defmodule ElixirSayHello.Mixfile do
 
   defp deps do
     [
-      {:credo, "~> 0.4", only: [:dev, :test]},
-      {:excoveralls, "~> 0.8", only: :test}
+      {:credo, git: "https://github.com/rrrene/credo.git", tag: "v1.7.12", only: [:dev, :test], runtime: false},
+      {:bunt, git: "https://github.com/rrrene/bunt.git", tag: "v1.0.0", only: [:dev, :test], override: true, runtime: false},
+      {:file_system, git: "https://github.com/falood/file_system.git", tag: "v1.1.1", only: [:dev, :test], override: true},
+      {:jason, git: "https://github.com/michalmuskala/jason.git", tag: "v1.4.4", only: [:dev, :test], override: true},
+      {:excoveralls, git: "https://github.com/parroty/excoveralls.git", tag: "v0.18.5", only: :test}
     ]
   end
 end
